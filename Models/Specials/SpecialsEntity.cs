b244bc00b7729792c85ee6a259fd2e2686349c46
林禄平 <ssrqm@163.com>
2015-06-24 22:48:23 +0800

    Add SpecialsEntity

---
+++ Models/Specials/SpecialsEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WitBird.XiaoChangHe.Models.Specials
+{
+    [Serializable]
+    public class SpecialsEntity
+    {
+        public Int32 Id { get; set; }
+        public Guid? ProductId { get; set; }
+        public Guid? RestaurantId { get; set; }
+        public Decimal? SpecPrice { get; set; }
+        public Int32? WeekDateUse { get; set; }
+        public Boolean? Stauts { get; set; }
+        public DateTime? UseDate { get; set; }
+    }
+}
---
+++ Models/Specials/SpecialsModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentData;
+
+namespace WitBird.XiaoChangHe.Models.Specials
+{
+    public class SpecialsModel
+    {
+        /// <summary>
+        /// 分页获取。
+        /// </summary>
+        /// <param name="startRowIndex"></param>
+        /// <param name="maximumRows"></param>
+        /// <param name="sortExpression"></param>
+        /// <returns></returns>
+        public static List<SpecialsEntity> SelectAll(int startRowIndex, int maximumRows, string sortExpression)
+        {
+            using (var context = 
+                new DbContext().ConnectionStringName("CrmRstV1", new SqlServerProvider()))
+            {
+                var select = context.Select<SpecialsEntity>(" * ")
+                    .From(" Specials ");
+
+                if (maximumRows > 0)
+                {
+                    if (startRowIndex == 0)
+                        startRowIndex = 1;
+
+                    select.Paging(startRowIndex, maximumRows);
+                }
+
+                if (!string.IsNullOrEmpty(sortExpression))
+                    select.OrderBy(sortExpression);
+
+                return select.QueryMany();
+            }
+        }
+
+        /// <summary>
+        /// 根据餐馆Id 得到特价菜数量。
+        /// </summary>
+        /// <param name="restaurantId"></param>
+        /// <returns></returns>
+        public static int CountAll(Guid restaurantId)
+        {
+            using (var context = new DbContext().ConnectionStringName("CrmRstV1", new SqlServerProvider()))
+            {
+                return context.Sql(" SELECT COUNT(*) FROM Specials ")
+                    .QuerySingle<int>();
+            }
+        }
+    }
+}
