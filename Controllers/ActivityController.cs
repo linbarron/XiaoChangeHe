34e5ee7b1e95ca668ffc0d4b51c8797ffd9fe818
harriszhang14 <harriszhang@live.cn>
2015-07-01 20:41:14 +0800

    add acticity

---
+++ Controllers/ActivityController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WitBird.XiaoChangHe.Controllers
+{
+    public class ActivityController : Controller
+    {
+        /// <summary>
+        /// 活动列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 活动详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
