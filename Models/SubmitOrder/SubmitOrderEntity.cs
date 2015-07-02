b3de279617eafa2b9c9a130f225192ddf9735a17
林禄平 <ssrqm@163.com>
2015-07-03 00:17:34 +0800

    update

---
+++ Models/SubmitOrder/SubmitOrderEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WitBird.XiaoChangHe.Models.SubmitOrder
+{
+    public class SubmitOrderEntity
+    {
+        public DateTime DiningDate { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public int? PersonCount { get; set; }
+        public Guid OrderId { get; set; }
+        public int? sex { set; get; }
+    }
+}
